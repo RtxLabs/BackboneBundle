a2e554ef20dc1a1f86351452b3d94c7eff788133
Uwe Klawitter <uwe_klawitter@web.de>
2012-11-12 17:55:09 +0100

    added assetic bundle to config

--- DependencyInjection/RtxLabsBackboneExtension.php
+++ DependencyInjection/RtxLabsBackboneExtension.php
@@ -24,8 +24,5 @@ class RtxLabsBackboneExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
     }
 }
